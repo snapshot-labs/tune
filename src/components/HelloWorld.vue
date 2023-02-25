87ffdce1f4ea8d3cad05830be8607b11fe7d2b90
Sam <samuv.eth@gmail.com>
2023-02-25 18:50:34 +0700

    css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,11 +27,5 @@ const count = ref(0);
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs p-5">Click on the Vite and Vue logos to learn more</p>
+  <p class="p-5 text-primary">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--primary-color))',
+        secondary: 'rgb(var(--secondary-color))',
+        success: 'rgb(var(--success-color))',
+        danger: 'rgb(var(--danger-color))',
+        warning: 'rgb(var(--warning-color))',
+        info: 'rgb(var(--info-color))',
+        light: 'rgb(var(--light-color))',
+        dark: 'rgb(var(--dark-color))',
+        'gray-light': 'rgb(var(--gray-light))',
+        'gray-medium': 'rgb(var(--gray-medium))',
+        'gray-dark': 'rgb(var(--gray-dark))'
+      },
+      borderRadius: {
+        default: 'var(--default-border-radius)',
+        button: 'var(--button-border-radius)'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
