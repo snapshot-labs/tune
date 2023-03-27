751d39c214f44432ceca81e3014e600c8ad933c6
Sam <samuv.eth@gmail.com>
2023-03-27 08:34:54 +0700

    Add icons

--- packages/tune/vite.config.ts
+++ packages/tune/vite.config.ts
@@ -3,13 +3,31 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import Components from "unplugin-vue-components/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   plugins: [
     vue(),
     dts(),
     Components({
-      /* options */
+      resolvers: [
+        IconsResolver({
+          customCollections: ["s"],
+          alias: {
+            hi: "heroicons",
+          },
+        }),
+      ],
+    }),
+    Icons({
+      compiler: "vue3",
+      customCollections: {
+        ti: FileSystemIconLoader("./src/assets/icons", (svg) =>
+          svg.replace(/^<svg /, '<svg fill="currentColor" ')
+        ),
+      },
     }),
   ],
   build: {
