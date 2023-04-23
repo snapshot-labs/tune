54ce54a8cd117c9fad29056966e19e8ecb6dc2d2
Sam <samuv.eth@gmail.com>
2023-04-23 17:10:25 +0700

    Fix rename

--- src/components/TuneCheckbox.story.vue
+++ src/components/TuneCheckbox.story.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import TuneInputCheckbox from './TuneInputCheckbox.vue';
+import TuneCheckbox from './TuneCheckbox.vue';
 
 const input = ref(false);
 </script>
 
 <template>
   <Story>
-    <TuneInputCheckbox v-model="input" label="Switch me" hint="I'm a checkbox" class="m-1" />
+    <TuneCheckbox v-model="input" label="Switch me" hint="I'm a checkbox" class="m-1" />
   </Story>
 </template>
--- src/components/TuneRadio.story.vue
+++ src/components/TuneRadio.story.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import TuneInputRadio from './TuneInputRadio.vue';
+import TuneRadio from './TuneRadio.vue';
 
 const input = ref('1');
 </script>
 
 <template>
   <Story>
-    <TuneInputRadio v-model="input" value="1" label="Switch me" hint="I'm radio 1" class="m-1" />
-    <TuneInputRadio v-model="input" value="2" label="Switch me" hint="I'm radio 2" class="m-1" />
+    <TuneRadio v-model="input" value="1" label="Switch me" hint="I'm radio 1" class="m-1" />
+    <TuneRadio v-model="input" value="2" label="Switch me" hint="I'm radio 2" class="m-1" />
   </Story>
 </template>
