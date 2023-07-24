1bf179280d79f46df2af3deb05dc35ae11494f14
Sam <samuv.eth@gmail.com>
2023-07-24 20:48:15 +0700

    Fix ButtonSelect and add variants from design

--- src/components/TuneButtonSelect.story.vue
+++ src/components/TuneButtonSelect.story.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
 import TuneButtonSelect from './TuneButtonSelect.vue';
+import IconCube from '~icons/heroicons-outline/cube';
 </script>
 
 <template>
-  <Story> <TuneButtonSelect model-value="Confirm" /> </Story>
+  <Story>
+    <Variant title="default">
+      <div class="space-y-3">
+        <TuneButtonSelect> Select </TuneButtonSelect>
+        <TuneButtonSelect>
+          <IconCube />
+          Select
+        </TuneButtonSelect>
+        <TuneButtonSelect> <IconCube /> </TuneButtonSelect>
+      </div>
+    </Variant>
+    <Variant title="without chevron">
+      <div class="space-y-3">
+        <TuneButtonSelect :show-chevron="false"> Select </TuneButtonSelect>
+        <TuneButtonSelect :show-chevron="false">
+          <IconCube />
+          Select
+        </TuneButtonSelect>
+        <TuneButtonSelect :show-chevron="false"> <IconCube /> </TuneButtonSelect>
+      </div>
+    </Variant>
+  </Story>
 </template>
