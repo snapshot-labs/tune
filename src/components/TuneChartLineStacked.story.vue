ef7130c1325aebd430d04c6b9fece0f2f48bcd5d
Sam <samuv.eth@gmail.com>
2023-07-24 19:54:37 +0700

    Add ChartLineStacked

---
+++ src/components/TuneChartLineStacked.story.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TuneChartLineStacked from './TuneChartLineStacked.vue';
+</script>
+
+<template>
+  <Story>
+    <div class="mt-3 w-[200px] space-y-3">
+      <TuneChartLineStacked :progress="[50, 25, 25]" />
+      <TuneChartLineStacked :progress="[75, 25, 0]" />
+      <TuneChartLineStacked :progress="[100, 0, 0]" />
+    </div>
+  </Story>
+</template>
