f9b3fdf62ea68b7a934c8e6df72d3199bf307203
Sam <samuv.eth@gmail.com>
2023-07-25 11:34:56 +0700

    Fix alert story

--- src/components/TuneAlert.story.vue
+++ src/components/TuneAlert.story.vue
@@ -5,13 +5,19 @@ import TuneAlert from './TuneAlert.vue';
 <template>
   <Story>
     <div class="space-y-[22px]">
-      <TuneAlert> The contracts have not yet undergone an audit. Use at own risk. </TuneAlert>
-      <TuneAlert variant="negative">
-        The contracts have not yet undergone an audit. Use at own risk.
-      </TuneAlert>
-      <TuneAlert variant="positive">
-        The contracts have not yet undergone an audit. Use at own risk.
-      </TuneAlert>
+      <Variant title="default">
+        <TuneAlert> The contracts have not yet undergone an audit. Use at own risk. </TuneAlert>
+      </Variant>
+      <Variant title="negative">
+        <TuneAlert variant="negative">
+          The contracts have not yet undergone an audit. Use at own risk.
+        </TuneAlert>
+      </Variant>
+      <Variant title="positive">
+        <TuneAlert variant="positive">
+          The contracts have not yet undergone an audit. Use at own risk.
+        </TuneAlert>
+      </Variant>
     </div>
   </Story>
 </template>
