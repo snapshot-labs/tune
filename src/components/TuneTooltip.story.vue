9f8e70b0e8635c53968d7d2b7be2d06a93c17b55
Sam <samuv.eth@gmail.com>
2023-07-26 17:59:42 +0700

    Add tooltip story

---
+++ src/components/TuneTooltip.story.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: 380 }">
+    <Variant title="default">
+      <div class="my-[100px] flex items-center justify-center">
+        <div v-tippy="{ content: 'Tooltip text', placement: 'top' }">
+          <HstButton class="htw-p-2">Hover me</HstButton>
+        </div>
+      </div>
+    </Variant>
+    <Variant title="show on click">
+      <div class="my-[100px] flex items-center justify-center">
+        <div v-tippy="{ content: 'Tooltip text', placement: 'top', trigger: 'click' }">
+          <HstButton class="htw-p-2">Click me</HstButton>
+        </div>
+      </div>
+    </Variant>
+    <Variant title="interactive">
+      <div class="my-[100px] flex items-center justify-center">
+        <div v-tippy="{ content: 'Tooltip text', placement: 'top', interactive: true }">
+          <HstButton class="htw-p-2">Hover me</HstButton>
+        </div>
+      </div>
+    </Variant>
+    <Variant title="show on create">
+      <div class="my-[100px] flex items-center justify-center">
+        <div v-tippy="{ content: 'Tooltip text', placement: 'top', showOnCreate: true }">
+          <HstButton class="htw-p-2">Hover me</HstButton>
+        </div>
+      </div>
+    </Variant>
+  </Story>
+</template>
