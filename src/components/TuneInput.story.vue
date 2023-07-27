23a114e6c529624341546112fcbf0053cffc1227
Sam <samuv.eth@gmail.com>
2023-07-27 07:22:46 +0700

    Fix missing prop

--- src/components/TuneInput.story.vue
+++ src/components/TuneInput.story.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import TuneInput from './TuneInput.vue';
+
+const input = ref('');
 </script>
 
 <template>
   <Story>
     <div class="w-[320px]">
-      <TuneInput label="Label" placeholder="Type something" />
+      <TuneInput v-model="input" label="Label" placeholder="Type something" />
     </div>
   </Story>
 </template>
