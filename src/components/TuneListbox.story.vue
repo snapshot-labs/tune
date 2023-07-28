6d2f471c0d5146c772963c13f41ec39b4467676d
Sam <samuv.eth@gmail.com>
2023-07-28 19:34:44 +0700

    Fix listbox

--- src/components/TuneListbox.story.vue
+++ src/components/TuneListbox.story.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import TuneListbox from './TuneListbox.vue';
 
 const items = [
@@ -9,12 +9,37 @@ const items = [
 ];
 
 const input = ref(1);
+
+const errorInputRef = ref();
+
+watchEffect(() => {
+  if (errorInputRef.value) errorInputRef.value?.forceShowError();
+});
 </script>
 
 <template>
-  <Story>
-    <div class="w-[320px]">
-      <TuneListbox v-model="input" :items="items" label="Label" />
-    </div>
+  <Story :layout="{ type: 'grid', width: 320 }">
+    <Variant title="default">
+      <TuneListbox
+        v-model="input"
+        :items="items"
+        label="Label"
+        class="mb-[200px]"
+        hint="Hint text"
+      />
+    </Variant>
+    <Variant title="disabled">
+      <TuneListbox v-model="input" :items="items" label="Label" class="mb-[200px]" disabled />
+    </Variant>
+    <Variant title="error">
+      <TuneListbox
+        ref="errorInputRef"
+        v-model="input"
+        :items="items"
+        label="Label"
+        class="mb-[200px]"
+        error="Select something"
+      />
+    </Variant>
   </Story>
 </template>
--- src/components/TuneListbox.vue
+++ src/components/TuneListbox.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import {
   Listbox,
   ListboxButton,
@@ -9,6 +9,7 @@ import {
 } from '@headlessui/vue';
 import isEqual from 'lodash/isEqual';
 import TuneLabelInput from './TuneLabelInput.vue';
+import TuneErrorInput from './TuneErrorInput.vue';
 import IconChevronDown from '~icons/heroicons-outline/chevron-down';
 import IconCheck from '~icons/heroicons-outline/check';
 
@@ -24,6 +25,7 @@ const props = defineProps<{
   label: string;
   disabled?: boolean;
   hint?: string;
+  error?: string;
 }>();
 
 const emit = defineEmits(['update:modelValue']);
@@ -33,77 +35,101 @@ const selectedItem = computed({
   set: newVal => emit('update:modelValue', newVal.value)
 });
 
+const showErrorMessage = ref(false);
+
+function forceShowError() {
+  showErrorMessage.value = true;
+}
+
+defineExpose({
+  forceShowError
+});
+
 const isDisabled = computed(() => (props.disabled ? 'tune-disabled-input' : ''));
 </script>
 
 <template>
-  <Listbox v-model="selectedItem" as="div" :disabled="disabled">
-    <div class="relative">
-      <ListboxButton
-        v-slot="{ open }"
-        class="tune-input-wrapper relative w-full truncate !pr-[40px] text-left"
-        :class="{ 'cursor-not-allowed': disabled }"
-      >
-        <ListboxLabel class="pointer-events-none" :class="isDisabled">
-          <TuneLabelInput :label="label" :hint="hint" />
-        </ListboxLabel>
-
-        <div :class="['tune-listbox-selected', isDisabled]">
-          <slot v-if="$slots.selected" name="selected" :selected-item="selectedItem" />
-          <span v-else-if="selectedItem">
-            {{ selectedItem?.name || selectedItem.value }}
-          </span>
-        </div>
-
-        <span class="absolute inset-y-[12px] right-[12px] flex items-end px-2" :class="isDisabled">
-          <IconChevronDown
-            :class="['tune-input-chevron text-base', { 'tune-input-chevron-up rotate-180': open }]"
-          />
-        </span>
-      </ListboxButton>
-      <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform -translate-y-2 scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-out"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
-      >
-        <ListboxOptions
-          class="tune-list absolute z-40 mt-1 w-full overflow-hidden focus:outline-none"
+  <div>
+    <Listbox v-model="selectedItem" as="div" :disabled="disabled">
+      <div class="relative">
+        <ListboxButton
+          v-slot="{ open }"
+          class="tune-input-wrapper relative w-full truncate !pr-[40px] text-left"
+          :class="[
+            { 'cursor-not-allowed': disabled },
+            {
+              error: showErrorMessage && error
+            }
+          ]"
         >
-          <div class="max-h-[180px] overflow-y-auto">
-            <ListboxOption
-              v-for="item in items"
-              :key="item.value"
-              v-slot="{ active, selected, disabled: itemDisabled }"
-              as="template"
-              :value="item"
-            >
-              <li
-                :class="[
-                  { active: active && !itemDisabled },
-                  'tune-list-item relative cursor-default select-none truncate !pr-[50px]'
-                ]"
-              >
-                <span :class="[{ 'opacity-40': itemDisabled }, 'block truncate']">
-                  <slot v-if="$slots.item" name="item" :item="item" />
-                  <span v-else>
-                    {{ item?.name || item.value }}
-                  </span>
-                </span>
+          <ListboxLabel class="pointer-events-none" :class="isDisabled">
+            <TuneLabelInput :label="label" :hint="hint" :error="!!error && showErrorMessage" />
+          </ListboxLabel>
 
-                <span
-                  v-if="selected"
-                  :class="['absolute inset-y-0 right-0 flex items-center pr-3']"
-                >
-                  <IconCheck class="text-base text-green" />
-                </span>
-              </li>
-            </ListboxOption>
+          <div :class="['tune-listbox-selected', isDisabled]">
+            <slot v-if="$slots.selected" name="selected" :selected-item="selectedItem" />
+            <span v-else-if="selectedItem">
+              {{ selectedItem?.name || selectedItem.value }}
+            </span>
           </div>
-        </ListboxOptions>
-      </transition>
-    </div>
-  </Listbox>
+
+          <span
+            class="absolute inset-y-[12px] right-[12px] flex items-end px-2"
+            :class="isDisabled"
+          >
+            <IconChevronDown
+              :class="[
+                'tune-input-chevron text-base',
+                { 'tune-input-chevron-up rotate-180': open }
+              ]"
+            />
+          </span>
+        </ListboxButton>
+        <transition
+          enter-active-class="transition duration-100 ease-out"
+          enter-from-class="transform -translate-y-2 scale-95 opacity-0"
+          enter-to-class="transform scale-100 opacity-100"
+          leave-active-class="transition duration-75 ease-out"
+          leave-from-class="transform scale-100 opacity-100"
+          leave-to-class="transform scale-95 opacity-0"
+        >
+          <ListboxOptions
+            class="tune-list absolute z-40 mt-1 w-full overflow-hidden focus:outline-none"
+          >
+            <div class="max-h-[180px] overflow-y-auto">
+              <ListboxOption
+                v-for="item in items"
+                :key="item.value"
+                v-slot="{ active, selected, disabled: itemDisabled }"
+                as="template"
+                :value="item"
+              >
+                <li
+                  :class="[
+                    { active: active && !itemDisabled },
+                    'tune-list-item relative cursor-default select-none truncate !pr-[50px]'
+                  ]"
+                >
+                  <span :class="[{ 'opacity-40': itemDisabled }, 'block truncate']">
+                    <slot v-if="$slots.item" name="item" :item="item" />
+                    <span v-else>
+                      {{ item?.name || item.value }}
+                    </span>
+                  </span>
+
+                  <span
+                    v-if="selected"
+                    :class="['absolute inset-y-0 right-0 flex items-center pr-3']"
+                  >
+                    <IconCheck class="text-base text-green" />
+                  </span>
+                </li>
+              </ListboxOption>
+            </div>
+          </ListboxOptions>
+        </transition>
+      </div>
+    </Listbox>
+    <TuneErrorInput v-if="error && showErrorMessage" :error="error" />
+  </div>
 </template>
