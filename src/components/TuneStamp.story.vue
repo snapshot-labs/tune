b086de57f04ad62746cbc173b7d4b238428e3ace
Sam <samuv.eth@gmail.com>
2023-07-30 09:16:31 +0700

    Add stamp

---
+++ src/components/TuneStamp.story.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import TuneStamp from './TuneStamp.vue';
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: 750 }">
+    <Variant title="avatar">
+      <div class="flex gap-3">
+        <TuneStamp id="0xE28142c9aBaF726dd92BF41be728Fe3220b34112" :size="80" />
+        <TuneStamp id="0xE28142c9aBaF726dd92BF41be728Fe3220b34112" :size="48" />
+        <TuneStamp id="0xE28142c9aBaF726dd92BF41be728Fe3220b34112" :size="32" />
+        <TuneStamp id="0xE28142c9aBaF726dd92BF41be728Fe3220b34112" set-favicon />
+        <TuneStamp id="0xE28142c9aBaF726dd92BF41be728Fe3220b34112" :size="18" />
+      </div>
+    </Variant>
+    <Variant title="space">
+      <div class="flex gap-3">
+        <TuneStamp id="testsnap6.eth" type="space" :size="80" />
+        <TuneStamp id="testsnap6.eth" type="space" :size="48" />
+        <TuneStamp id="testsnap6.eth" type="space" :size="32" />
+        <TuneStamp id="testsnap6.eth" type="space" />
+        <TuneStamp id="testsnap6.eth" type="space" :size="18" />
+      </div>
+    </Variant>
+    <Variant title="sx-cover">
+      <TuneStamp
+        id="0x0f4504ccd7f98368a0f94cde1ade1b624635ffe2"
+        :size="{ width: 750, height: 78 }"
+        type="space-cover-sx"
+      />
+    </Variant>
+    <Variant title="token">
+      <div class="flex gap-3">
+        <TuneStamp id="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" type="token" :size="80" />
+        <TuneStamp id="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" type="token" :size="48" />
+        <TuneStamp id="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" type="token" :size="32" />
+        <TuneStamp id="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" type="token" />
+        <TuneStamp id="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" type="token" :size="18" />
+      </div>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/TuneStamp.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { computed, watchEffect } from 'vue';
+import sha3 from 'js-sha3';
+import { useFavicon } from '@vueuse/core';
+
+const props = withDefaults(
+  defineProps<{
+    type?: 'avatar' | 'space' | 'space-sx' | 'space-cover-sx' | 'token';
+    id: string;
+    size?: number | { width: number; height: number };
+    cacheBreaker?: string;
+    setFavicon?: boolean;
+  }>(),
+  {
+    type: 'avatar',
+    size: 24
+  }
+);
+
+const sizeParam = computed(() => {
+  if (typeof props.size === 'number') {
+    return `?s=${props.size * 2}`;
+  } else {
+    return `?w=${props.size.width}&h=${props.size.height}`;
+  }
+});
+
+const imageUrl = computed(() => {
+  const cacheParam = props.cacheBreaker ? `&cb=${getCacheHash(props.cacheBreaker)}` : '';
+
+  return `https://cdn.stamp.fyi/${props.type}/${props.id}${sizeParam.value}${cacheParam}`;
+});
+
+const imageStyle = computed(() => {
+  if (typeof props.size === 'number') {
+    return {
+      width: `${props.size}px`,
+      height: `${props.size}px`
+    };
+  } else {
+    return {
+      width: `${props.size.width}px`,
+      height: `${props.size.height}px`
+    };
+  }
+});
+
+const imgAlt = computed(() => {
+  if (props.type === 'space' || props.type === 'space-sx') {
+    return 'Space avatar';
+  } else if (props.type === 'space-cover-sx') {
+    return 'Space cover';
+  } else if (props.type === 'token') {
+    return 'Token icon';
+  } else {
+    return 'Avatar';
+  }
+});
+
+const imgClasses = computed(() => {
+  return [
+    {
+      'avatar-style': props.type === 'avatar',
+      'cover-style': props.type === 'space-cover-sx'
+    },
+    props.type === 'space' || props.type === 'space-sx'
+      ? typeof props.size === 'number' && props.size >= 48
+        ? 'space-style-lg'
+        : 'space-style'
+      : null
+  ];
+});
+
+function getCacheHash(value?: string) {
+  return value ? sha3.sha3_256(value).slice(0, 16) : undefined;
+}
+
+const icon = useFavicon();
+
+watchEffect(() => {
+  if (props.setFavicon) {
+    if (typeof props.size !== 'number')
+      throw new Error('Size must be a number when setting favicon');
+    const faviconUrl = imageUrl.value.replace(sizeParam.value, '?s=16');
+    icon.value = faviconUrl;
+  }
+});
+</script>
+
+<template>
+  <img :src="imageUrl" :style="imageStyle" :alt="imgAlt" :class="['tune-stamp', imgClasses]" />
+</template>
